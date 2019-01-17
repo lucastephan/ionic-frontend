9a13b389fd3b3e45c0f5ce5e3757c29cc6739c9a
lucastephan <lucastephann@gmail.com>
2019-01-17 00:05:36 -0200

    Fazendo o app aproveitar o usuário logado (utilizando RefreshToken)

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -22,7 +22,14 @@ export class AuthService {
     return this.http.post(`${API_CONFIG.baseUrl}/login`, creds, {
       observe: 'response',
       responseType: 'text'
-    })
+    });
+  }
+
+  refreshToken() {
+    return this.http.post(`${API_CONFIG.baseUrl}/auth/refresh_token`, {}, {
+      observe: 'response',
+      responseType: 'text'
+    });
   }
 
   successfulLogin(authorizationValue : string) {
